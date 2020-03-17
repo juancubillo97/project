d97177ae55884ad57681fa9e3a6d48418f792fc9
juancubillos <juan.cubillos@udla.edu.co>
2020-03-17 17:23:02 -0500

    init

---
+++ src/common/common.module.ts
@@ -0,0 +1,21 @@
+import { Module, Global } from '@nestjs/common';
+import { TypeOrmConfigService } from './providers/ormconfig.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/enviroment.config'
+
+
+
+@Global()
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      envFilePath: process.cwd()+'/environments/development.env',
+      isGlobal: true,
+    }),
+
+  ],
+  providers: [TypeOrmConfigService], 
+  exports: [TypeOrmConfigService, ]
+})
+export class CommonModule {}
